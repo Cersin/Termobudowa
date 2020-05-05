88339d2ccde8d1fb3049e76dd6f1ddf7bae4826c
Cersin <cersin96@gmail.com>
2020-05-05 11:58:59 +0200

    navbar

---
+++ src/components/navbar.vue
@@ -0,0 +1,71 @@
+<template>
+    <nav>
+        <v-app-bar short app flat class="primary">
+
+            <v-toolbar-title class="title text-uppercase black--text font-weight-light">
+                <span >Termo</span>
+                <span class="font-weight-bold">budowa</span>
+            </v-toolbar-title>
+
+                <!-- odstęp w nawigacji !-->
+                <v-spacer></v-spacer>
+
+                <!--Przyciski na med i wys rozdzielczosci !-->
+                <v-btn v-for="link in links" :key="link.text" small text class="text--secondary d-none d-md-block">
+                    <span>{{ link.text }}</span>
+                    <v-icon right>{{ link.icon }}</v-icon>
+                </v-btn>
+
+                <!--Przyciski ponizej med rozdzielczosci !-->
+            <template>
+                <div class="text-center d-md-none">
+                    <v-menu offset-y>
+                        <template v-slot:activator="{ on }">
+                            <v-btn small v-on="on" rounded class="transparent white--text" >
+                                <span>Menu</span>
+                                <v-icon>mdi-chevron-down</v-icon>
+                            </v-btn>
+                        </template>
+                        <v-list class="primary lighten-2">
+                            <v-list-item
+                                    v-for="link in links"
+                                    :key="link.text"
+                            >
+                                <span>
+                                <v-icon>{{ link.icon }}</v-icon>
+                                </span>
+                                <span>
+                                <v-list-item-title>{{ link.text }}</v-list-item-title>
+                                </span>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
+                </div>
+            </template>
+
+        </v-app-bar>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "navbar",
+        data() {
+            return{
+                links: [
+                    { icon: 'mdi-home', text: 'Strona Główna', route:'/'},
+                    { icon: 'mdi-home', text: 'Strona Główna', route:'/'},
+                    { icon: 'mdi-home', text: 'Strona Główna', route:'/'},
+                    { icon: 'mdi-home', text: 'Strona Główna', route:'/'},
+                    { icon: 'mdi-home', text: 'Strona Główna', route:'/'},
+
+
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,18 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme:      {
+        primary: '#3f51b5',
+        secondary: '#2196f3',
+        accent: '#03a9f4',
+        error: '#f44336',
+        warning: '#9c27b0',
+        info: '#ffc107',
+        success: '#8bc34a'
+    },
+    icons: {
+        iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    },
+
+
 });
