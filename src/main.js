ad11ad4940a9ff58055fadc1749bfa029e632a82
Cersin <cersin96@gmail.com>
2020-05-05 14:13:07 +0200

    animation-test

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="home">
 <company/>
+    <company data-aos="fade-left"
+             data-aos-delat="150"
+             data-aos-duration="1000"
+             data-aos-easing="ease-in-out"
+             data-aos-mirror="true"
+             data-aos-once="false"/>
+    <company data-aos="fade-right"
+             data-aos-delat="150"
+             data-aos-duration="1000"
+             data-aos-easing="ease-in-out"
+             data-aos-mirror="true"
+             data-aos-once="false"/>
+    <company data-aos="fade-up"
+             data-aos-delat="150"
+             data-aos-duration="1000"
+             data-aos-easing="ease-in-out"
+             data-aos-mirror="true"
+             data-aos-once="false"/>
+    <company data-aos="fade-down"
+             data-aos-delat="150"
+             data-aos-duration="1000"
+             data-aos-easing="ease-in-out"
+             data-aos-mirror="true"
+             data-aos-once="false"/>
 
   </div>
 </template>
