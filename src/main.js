203f57595d6c9946503d9195c4f36e50e712766e
Cersin <cersin96@gmail.com>
2020-05-13 12:16:29 +0200

    Tinybox

--- src/main.js
+++ src/main.js
@@ -4,11 +4,12 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Tinybox from "vue-tinybox";
 AOS.init({ disable: 'mobile' });
 Vue.config.productionTip = false
-
 new Vue({
   router,
   vuetify,
+  Tinybox,
   render: h => h(App)
 }).$mount('#app')
