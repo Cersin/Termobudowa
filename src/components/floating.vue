622adfa0195323f62e7ccb92565730a025e1f081
Cersin <cersin96@gmail.com>
2020-05-21 07:34:05 +0200

    reference fix

---
+++ src/components/floating.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-navigation-drawer floating  fixed class="transparent floating_drawer"   >
+                 <span>
+                <v-btn
+                        block
+                        color="primary"
+                        @click="$vuetify.goTo('#about',{duration:1000})"
+                >O nas</v-btn>
+                <v-btn
+                        block
+                        color="primary"
+                        @click="$vuetify.goTo('#contact',{duration:1000})"
+                >Kontakt</v-btn>
+                <v-btn
+                        block
+                        color="primary"
+                        @click="$vuetify.goTo('#googlemap',{duration:1000})"
+                >Lokalizacja</v-btn>
+            </span>
+    </v-navigation-drawer>
+
+</template>
+
+<script>
+    export default {
+        name: "floating"
+    }
+</script>
+
+<style scoped>
+.floating_drawer {
+    height: 300px
+}
+
+</style>
